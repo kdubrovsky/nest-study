27d959781b0df86fdc6cde06d3bede2312b9cf4c
Konstantin Tereschenkov <konstantin.tereschenkov@trustform.io>
2024-12-30 15:40:55 +0400

    feat: Add dto global validation pipe

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,5 +1,17 @@
+import { IsNotEmpty, IsNumber, Max, Min, MinLength } from 'class-validator';
+
 export class CreateBookDto {
+  @IsNotEmpty()
+  @MinLength(2)
   title: string;
+
+  @IsNotEmpty()
+  @MinLength(2)
   author: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(1800)
+  @Max(new Date().getFullYear())
   year: number;
 }
--- src/books/dto/update-book.dto.ts
+++ src/books/dto/update-book.dto.ts
@@ -1,5 +1,14 @@
+import { IsNumber, Max, Min, MinLength } from 'class-validator';
+
 export class UpdateBookDto {
+  @MinLength(2)
   title?: string;
+
+  @MinLength(2)
   author?: string;
+
+  @IsNumber()
+  @Min(1800)
+  @Max(new Date().getFullYear())
   year?: number;
 }
